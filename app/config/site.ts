344eedc7c26a08b3f7972937c19d0af2353f90bb
Zhiyao <15222848787@163.com>
2024-08-01 01:29:20 -0400

    init commit

---
+++ app/config/site.ts
@@ -0,0 +1,13 @@
+import { SiteConfig } from "types"
+
+export const siteConfig: SiteConfig = {
+  name: "w-app",
+  description:
+    "An open source application built using the new router, server components and everything new in Next.js.",
+  url: "",
+  ogImage: "",
+  links: {
+    twitter: "https://x.com/WWWWzy3",
+    github: "https://github.com/shadcn/taxonomy",
+  },
+}
---
+++ app/layout.tsx
@@ -0,0 +1,66 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import localFont from "next/font/local";
+import "./globals.css";
+
+const fontSans = Inter({
+  subsets : ["latin"],
+  variable: "--font-sans",
+});
+
+
+const fontHeading = localFont({
+  src: "../styles/assets/fonts/CalSans-SemiBold.woff2",
+  variable: "--font-heading",
+})
+
+interface RootLayoutProps {
+  children: React.ReactNode
+}
+
+export const metadata = {
+  title: {
+    default: siteConfig.name,
+    template: `%s | ${siteConfig.name}`,
+  },
+  description: siteConfig.description,
+  keywords: [
+    "Next.js",
+    "React",
+    "Tailwind CSS",
+    "Server Components",
+    "Radix UI",
+  ],
+  authors: [
+    {
+      name: "shadcn",
+      url: "https://shadcn.com",
+    },
+  ],
+  creator: "shadcn",
+  themeColor: [
+    { media: "(prefers-color-scheme: light)", color: "white" },
+    { media: "(prefers-color-scheme: dark)", color: "black" },
+  ],
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: siteConfig.url,
+    title: siteConfig.name,
+    description: siteConfig.description,
+    siteName: siteConfig.name,
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: siteConfig.name,
+    description: siteConfig.description,
+    images: [`${siteConfig.url}/og.jpg`],
+    creator: "@shadcn",
+  },
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon-16x16.png",
+    apple: "/apple-touch-icon.png",
+  },
+  manifest: `${siteConfig.url}/site.webmanifest`,
+}
