9950d8a1a36b520de0a78795057928b06faa249d
Zhiyao <15222848787@163.com>
2024-08-08 22:35:57 -0700

    add marketing

---
+++ app/(marketing)/layout.tsx
@@ -0,0 +1,41 @@
+import Link from "next/link"
+
+import { marketingConfig } from "@/config/marketing"
+import { cn } from "@/lib/utils"
+import { buttonVariants } from "@/components/ui/button"
+import { MainNav } from "@/components/main-nav"
+import { SiteFooter } from "@/components/site-footer"
+import { Suspense } from "react"
+
+interface MarketingLayoutProps {
+  children: React.ReactNode
+}
+
+export default async function MarketingLayout({
+  children,
+}: MarketingLayoutProps) {
+  return (
+    <div className="flex min-h-screen flex-col">
+        <Suspense >
+      <header className="container z-40 bg-background">
+        <div className="flex h-20 items-center justify-between py-6">
+          <MainNav items={marketingConfig.mainNav} />
+          <nav>
+            <Link
+              href="/login"
+              className={cn(
+                buttonVariants({ variant: "secondary", size: "sm" }),
+                "px-4"
+              )}
+            >
+              Login
+            </Link>
+          </nav>
+        </div>
+      </header>
+        </Suspense>
+      <main className="flex-1">{children}</main>
+      <SiteFooter />
+    </div>
+  )
+}
