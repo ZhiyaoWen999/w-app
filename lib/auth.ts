a67df24696c3beed7d963df89fa273c2328dcac7
Zhiyao <15222848787@163.com>
2024-08-22 20:23:25 -0700

    setting database

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,13 +1,60 @@
-import NextAuth from "next-auth"
-import GitHub from "next-auth/providers/github"
 
-export const authOptions = {
+import { PrismaAdapter } from "@auth/prisma-adapter";
+
+import {NextAuthOptions , type DefaultSession } from "next-auth";
+import GitHub from "next-auth/providers/github"
+import { prisma } from "@/lib/db";
+import { getUserById } from "@/lib/user";
+
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma) as any,
+  session: { strategy: "jwt" },
+  pages: {
+    signIn: "/login",
+    // error: "/auth/error",
+  },
+  callbacks: {
+    async session({ token, session }) {
+      if (session.user) {
+        if (token.sub) {
+          session.user.id = token.sub;
+        }
+
+        if (token.email) {
+          session.user.email = token.email;
+        }
+
+        if (token.role) {
+          session.user.role = token.role;
+        }
+
+        session.user.name = token.name;
+        session.user.image = token.picture;
+      }
+
+      return session;
+    },
+
+    async jwt({ token }) {
+      if (!token.sub) return token;
+
+      const dbUser = await getUserById(token.sub);
+
+      if (!dbUser) return token;
+
+      token.name = dbUser.name;
+      token.email = dbUser.email;
+      token.picture = dbUser.image;
+      token.role = dbUser.role;
+
+      return token;
+    },
+  },
   providers: [
     GitHub({
       clientId: process.env.GITHUB_CLIENT_ID as string,
       clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
     })
   ],
-}
-
-export const handlers = NextAuth(authOptions)
+  // debug: process.env.NODE_ENV !== "production"
+};
